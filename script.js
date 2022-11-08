729116f2648ad103944cdbd02ed8daaf3a6b7420
winner umeh <winniffy@gmail.com>
2022-11-08 10:22:25 +0100

    add button functionality

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-// 'use script'
+'use script'
 
-// const carouselImages = document.querySelector('.carousel_images');
-
+// select buttons
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
-// const numberOfImages = document.querySelectorAll('.carousel_images img').length;
+
+// select all slides
 const slides = document.querySelectorAll('.slide');
+
+// set current slide
 let curSlide = 0;
+
+// max no. of slides
 let maxSlide = slides.length - 1;
 
-
+// loop slides and translate all by 500px each
 slides.forEach((slide, index) => {
     slide.style.transform = `translateX(${index * 500}px)`;
 });
 
+// next btn functionality
 nextBtn.addEventListener('click', function() {
 
+    // check if current slide is last slide
     if (curSlide === maxSlide) {
         curSlide = 0;
     } else {
         curSlide++;
     }
 
+    // translate slide by -500px
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${500 * (index - curSlide)}px)`;
+    });
+});
+
+// previous btn functionality
+prevBtn.addEventListener('click', function() {
+
+    // check if current slide is first slide
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+
+    // translate slide by +500px
     slides.forEach((slide, index) => {
         slide.style.transform = `translateX(${500 * (index - curSlide)}px)`;
     });
